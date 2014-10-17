5156cef6e3fb31bdf0b6309cda8d4225468c5ad4
Jim Bergman <jdev@imagenuity.com>
2014-10-17 13:47:07 -0700

    recognizes iPod, returns device type, refactored
    tested up to iOS 8.0.x and Android 4.4.x

--- README.md
+++ README.md
@@ -3,19 +3,25 @@ getMobileOSver.js
 
 A bit of JavaScript to determine iOS and Android OS version.
 
-Two global vars mobileOS & mobileOSver contain the client's OS and version after a call to getMobileOS();
+Sets 3 vars:
 
-getMobileOSver-useragent-test.html runs tests with actual user agent strings for iOS 4.3 to 6.0.1, and Android 2.3.4 to 4.2
+    getMobileOS.os      is 'ios' | 'android' | false
+    getMobileOS.device  is 'iPad' | 'iPod' | 'iPhone' | 'Android'
+    getMobileOS.ver     is major.minor version as a string e.g. '4.1' or '8.0'
+                        ( use Number(mobileOSver) to convert )
+
+see code for notes on backwards compatibility issues with v1.00
+
+getMobileOSver-useragent-test.html runs tests with real user agent strings for iOS 4.3 to 8.0.2, and Android 2.3.4 to 4.4.4
 
 Example:
 
-&lt;script src="getMobileOSver.js"&gt;&lt;/script&gt;
+    <script src="getMobileOSver.js"></script>
 
-...
+    ...
 
-getMobileOS();
+    getMobileOS.init();
 
-if ( userOS === 'iOS' && Number( userOSver.charAt(0) ) &gt;= 5 )
-{
-  ...; // your code here
-}
+    if ( getMobileOS.os === 'ios' && Number( getMobileOS.ver ) >= 5 ) {
+      ...; // your code here
+    }
--- getMobileOSver.js
+++ getMobileOSver.js
@@ -1,46 +1,47 @@
 //
-// getMobileOS
+// getMobileOSver
 //
 //   written by Jim Bergman
 //   PUBLIC DOMAIN
 //
 // v1.00 - 2012-Nov-24
+// v1.10 - 2014-Oct-17 - recognizes iPod and refactored
 
-var mobileOS;    // will either be iOS, Android or unknown
-var mobileOSver; // this is a string, use Number(mobileOSver) to convert
+// uncomment these for v1.00 compatibility
+// var mobileOS    = getMobileOS.os;   // will either be iOS, Android or unknown (NOTE: lower case in v1.10 breaks compatibility, is now: ios, android or false)
+// var mobileOSver = getMobileOS.ver;  // this is a string, use Number(mobileOSver) to convert
 
-function getMobileOS( )
-{
-  var ua = navigator.userAgent;
-  var uaindex;
+// getMobileOS.os      is ios, android or false
+// getMobileOS.device  is iPad iPod iPhone or Android
+// getMobileOS.ver     is the OS major.minor version as a string e.g. '4.1' or '8.0' ( use Number(mobileOSver) to convert )
+var getMobileOS = {
+  init: function ( p ) {
+    var ua, uaindex;
 
-  // determine OS
-  if ( ua.match(/iPad/i) || ua.match(/iPhone/i) )
-  {
-    mobileOS = 'iOS';
-    uaindex  = ua.indexOf( 'OS ' );
-  }
-  else if ( ua.match(/Android/i) )
-  {
-    mobileOS = 'Android';
-    uaindex  = ua.indexOf( 'Android ' );
-  }
-  else
-  {
-    mobileOS = 'unknown';
-  }
+    if ( p ) { ua = p } else { ua = navigator.userAgent }
 
-  // determine version
-  if ( mobileOS === 'iOS'  &&  uaindex > -1 )
-  {
-    mobileOSver = ua.substr( uaindex + 3, 3 ).replace( '_', '.' );
+    if      ( ua.match(/iPad/i) )    { getMobileOS.device = 'iPad';     getMobileOS.os = 'ios';      uaindex = ua.indexOf( 'OS ' ); }
+    else if ( ua.match(/iPod/i) )    { getMobileOS.device = 'iPod';     getMobileOS.os = 'ios';      uaindex = ua.indexOf( 'OS ' ); } // must come before iPhone line
+    else if ( ua.match(/iPhone/i) )  { getMobileOS.device = 'iPhone';   getMobileOS.os = 'ios';      uaindex = ua.indexOf( 'OS ' ); }
+    else if ( ua.match(/Android/i) ) { getMobileOS.device = 'Android';  getMobileOS.os = 'android';  uaindex = ua.indexOf( 'Android ' ); }
+    else                             { getMobileOS.device = 'unknown';  getMobileOS.os = false; }
+
+    // see file getMobileOSver-useragent-test.html for sample navigator.userAgent strings
+
+    if ( getMobileOS.os === 'ios'  &&  uaindex > -1 ) {
+      getMobileOS.ver = ua.substr( uaindex + 3, 3 ).replace( '_', '.' );
+      // console.log( 'getMobileOS.ver: iOS v' + getMobileOS.ver );
+      return true;
+    }
+    else if ( getMobileOS.os === 'android' ) {
+      getMobileOS.ver = ua.substr( uaindex + 8, 3 );
+      // console.log( 'getMobileOS.ver: Android v' + getMobileOS.ver );
+      return true;
+    }
+    else {
+      // console.log( 'getMobileOS.os: ' + getMobileOS.os );
+      getMobileOS.ver = false;
+      return false;
+    }
   }
-  else if ( mobileOS === 'Android'  &&  uaindex > -1 )
-  {
-    mobileOSver = ua.substr( uaindex + 8, 3 );
-  }
-  else
-  {
-    mobileOSver = 'unknown';
-  }
-}
+};
