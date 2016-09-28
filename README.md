fd7a4ea0b6087f3384e746f435ecd1411530bbb9
Jim Bergman <jdev@imagenuity.com>
2016-09-28 14:39:17 -0700

    v1.2 - fixes two digit OS bug, adds Windows Phone and more
    adds major minor patch version breakout & device flags .android .ios
    .ipad .ipod .iphone .windowsphone

--- README.md
+++ README.md
@@ -1,27 +1,65 @@
-getMobileOSver.js
-=================
+# getMobileOSverJS
 
-A bit of JavaScript to determine iOS and Android OS version.
+A bit of JavaScript to determine OS version from Android, iOS and Windows Phone devices.
 
-Sets 3 vars:
+Extracts information from the UserAgent string in the browser:
 
-    getMobileOS.os      is 'ios' | 'android' | false
-    getMobileOS.device  is 'iPad' | 'iPod' | 'iPhone' | 'Android'
-    getMobileOS.ver     is major.minor version as a string e.g. '4.1' or '8.0'
-                        ( use Number(mobileOSver) to convert )
+    getMobileOS.device  is 'Android' | 'iPad' | 'iPod' | 'iPhone' | 'Windows Phone' | '' if unknown
+    getMobileOS.os      is 'android' | 'ios' | 'windowsphone' | false if unknown
 
-see code for notes on backwards compatibility issues with v1.00
+    getMobileOS.ver     is major.minor.patch version as a string e.g. '4.4.4' or '10.0' | '' if unknown
+    getMobileOS.major   is the numeric major version, i.e. in '9.3.6' it would be 9 | 0 if unknown
+    getMobileOS.minor   is the numeric minor version, i.e. in '9.3.6' it would be 3 | 0 if unknown
+    getMobileOS.patch   is the numeric patch version, i.e. in '9.3.6' it would be 6 | 0 if unknown
 
-getMobileOSver-useragent-test.html runs tests with real user agent strings for iOS 4.3 to 8.0.2, and Android 2.3.4 to 4.4.4
+    getMobileOS.android true if Android, all others (.ios .ipad .ipod .iphone .windowsphone) | false
+    getMobileOS.ios     true if iOS and also one of these will be true [.ipad .ipod .iphone] | false
+    getMobileOS.ipad    true if iPad | false
+    getMobileOS.ipod    true if iPod | false
+    getMobileOS.iphone  true if iPhone | false
+    getMobileOS.windowsphone  true if Windows Phone | false
 
-Example:
+Small, lightweight, and mobile friendly. Tested on iOS 4.3+, Android 2.3+ and Windows Phone 8+.
+
+Works great with Cordova/PhoneGap applications.
+
+## Requirements
+
+Include these in your HTML:
 
     <script src="getMobileOSver.js"></script>
 
-    ...
+or use the minified version:
+
+    <script src="getMobileOSver.min.js"></script>
+
+## Example
 
     getMobileOS.init();
 
-    if ( getMobileOS.os === 'ios' && Number( getMobileOS.ver ) >= 5 ) {
+    if ( getMobileOS.ios && getMobileOS.major >= 9 ) {
       ...; // your code here
     }
+
+## Dependencies
+
+None. All code is vanilla JavaScript v5.
+
+## Unit Test Data
+
+getMobileOSver-useragent-test.html runs tests with real user agent strings for Android 2.3.4 to 7.0, iOS 4.3 to 10.0.1 and Windows Phone 8.0 to 10.0 (all the latest as of this push date).
+
+## Live Demo
+
+http://jimbergman.net/scripts/getMobileOSverJS/
+
+## version 1.0 compatibility
+
+    // add these for v1.00 compatibility
+    // (NOTE: lower case in v1.10+ breaks compatibility, is now: ios | android | windowsphone | false)
+    var mobileOS    = getMobileOS.os;
+    var mobileOSver = getMobileOS.ver;
+
+## License
+
+Licensed under the [MIT License](http://opensource.org/licenses/MIT). Please refer to the LICENSE file.
--- getMobileOSver.js
+++ getMobileOSver.js
@@ -1,48 +1,82 @@
 //
 // getMobileOSver
 //   written by Jim Bergman
-//   PUBLIC DOMAIN
+//   MIT License
 //
-//  https://github.com/jimbergman/getMobileOSver.js
+//  https://github.com/jimbergman/getMobileOSverJS
 //
-// v1.00 - 2012-Nov-24
-// v1.10 - 2014-Oct-17 - recognizes iPod and refactored
+// v1.00 2012-Nov-24 initial release
+// v1.10 2014-Oct-17 recognizes iPod and refactored
+// v1.20 2016-Sep-28 bug fix for major OS version being two digits (i.e. 10 or higher) & Windows Phone support
+//                   major, minor and patch version available & new flags .android .ios .ipad .ipod .iphone .windowsphone
 
-// uncomment these for v1.00 compatibility
-// var mobileOS    = getMobileOS.os;   // will either be iOS, Android or unknown (NOTE: lower case in v1.10 breaks compatibility, is now: ios, android or false)
-// var mobileOSver = getMobileOS.ver;  // this is a string, use Number(mobileOSver) to convert
-
-// getMobileOS.os      is ios, android or false
-// getMobileOS.device  is iPad iPod iPhone or Android
-// getMobileOS.ver     is the OS major.minor version as a string e.g. '4.1' or '8.0' ( use Number(mobileOSver) to convert )
 var getMobileOS = {
+
   init: function ( p ) {
-    var ua, uaindex;
-
-    if ( p ) { ua = p } else { ua = navigator.userAgent }
-
-    if      ( ua.match(/iPad/i) )    { getMobileOS.device = 'iPad';     getMobileOS.os = 'ios';      uaindex = ua.indexOf( 'OS ' ); }
-    else if ( ua.match(/iPod/i) )    { getMobileOS.device = 'iPod';     getMobileOS.os = 'ios';      uaindex = ua.indexOf( 'OS ' ); } // must come before iPhone line
-    else if ( ua.match(/iPhone/i) )  { getMobileOS.device = 'iPhone';   getMobileOS.os = 'ios';      uaindex = ua.indexOf( 'OS ' ); }
-    else if ( ua.match(/Android/i) ) { getMobileOS.device = 'Android';  getMobileOS.os = 'android';  uaindex = ua.indexOf( 'Android ' ); }
-    else                             { getMobileOS.device = 'unknown';  getMobileOS.os = false; }
+    var ua, uaindex, uaindexend;
+    'use strict';
 
     // see file getMobileOSver-useragent-test.html for sample navigator.userAgent strings
+    if ( p ) { ua = p } else { ua = navigator.userAgent }
 
-    if ( getMobileOS.os === 'ios'  &&  uaindex > -1 ) {
-      getMobileOS.ver = ua.substr( uaindex + 3, 3 ).replace( '_', '.' );
-      // console.log( 'getMobileOS.ver: iOS v' + getMobileOS.ver );
-      return true;
+    // reset the device booleans
+    getMobileOS.android = getMobileOS.ios = getMobileOS.ipad = getMobileOS.ipod = getMobileOS.iphone = getMobileOS.windowsphone = false;
+
+    // determine device type and set the things
+    if ( ua.match(/iPad/i) ) {
+      uaindex = ua.indexOf( 'OS ' ) + 3;
+      uaindexend = ua.indexOf( ' ', uaindex );
+      getMobileOS.ios = getMobileOS.ipad = true;
+      getMobileOS.setAllTheThings( ua, 'iPad', 'ios', uaindex, uaindexend );
     }
-    else if ( getMobileOS.os === 'android' ) {
-      getMobileOS.ver = ua.substr( uaindex + 8, 3 );
-      // console.log( 'getMobileOS.ver: Android v' + getMobileOS.ver );
-      return true;
+    else if ( ua.match(/iPod/i) ) {
+      // must come before iPhone
+      uaindex = ua.indexOf( 'OS ' ) + 3;
+      uaindexend = ua.indexOf( ' ', uaindex );
+      getMobileOS.ios = getMobileOS.ipod = true;
+      getMobileOS.setAllTheThings( ua, 'iPod', 'ios', uaindex, uaindexend );
+    }
+    else if ( ua.match(/iPhone/i) ) {
+      // must come after iPod
+      uaindex = ua.indexOf( 'OS ' ) + 3;
+      uaindexend = ua.indexOf( ' ', uaindex );
+      getMobileOS.ios = getMobileOS.iphone = true;
+      getMobileOS.setAllTheThings( ua, 'iPhone', 'ios', uaindex, uaindexend );
+    }
+    else if ( ua.match(/Windows Phone/i) ) {
+      // must come before Android
+      uaindex = ua.indexOf( 'Windows Phone ' ) + 14;
+      uaindexend = ua.indexOf( ';', uaindex );
+      getMobileOS.windowsphone = true;
+      getMobileOS.setAllTheThings( ua, 'Windows Phone', 'windowsphone', uaindex, uaindexend );
+    }
+    else if ( ua.match(/Android/i) ) {
+      // must come after Windows Phone
+      uaindex = ua.indexOf( 'Android ' ) + 8;
+      uaindexend = ua.indexOf( ';', uaindex );
+      getMobileOS.android = true;
+      getMobileOS.setAllTheThings( ua, 'Android', 'android', uaindex, uaindexend );
     }
     else {
-      // console.log( 'getMobileOS.os: ' + getMobileOS.os );
-      getMobileOS.ver = false;
-      return false;
+      getMobileOS.setAllTheThings( '' );
+    }
+
+  },
+
+  setAllTheThings: function ( userAgent, device, os, i, e ) {
+    'use strict';
+    getMobileOS.device = device || '';
+    getMobileOS.os = os || false;
+    getMobileOS.ver = userAgent.substr( i, ( e - i ) ) || '';
+    var temp = getMobileOS.ver.split( /[_\.]+/ );   // iOS versions use '_' between numbers, Android and Windows use '.'
+    getMobileOS.major = temp[0] || 0;
+    getMobileOS.minor = temp[1] || 0;
+    getMobileOS.patch = temp[2] || 0;
+
+    // the funky Apple uses underscores in their version string instead of period like Android and Windows Phone
+    if ( getMobileOS.ios ) {
+      getMobileOS.ver = getMobileOS.ver.replace( /_/g, '.' );
     }
   }
+
 };
